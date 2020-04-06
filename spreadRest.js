e410d5e8ff50ee332e1eac6deb592659bed5beaa
shalinivenugopal <shalinikamini29@gmail.com>
2020-04-06 19:14:50 +0530

    Spread and Rest opreators

--- spreadRest.js
+++ spreadRest.js
@@ -1,16 +1,36 @@
-let actors = ['Vijay','SivaKarthikeyan','Atharva'];
-
-let updatedActor = [...actors, 'Dhanush','Suriya'];     // ... => this indicates spread operator
-console.log(actors);
-console.log(updatedActor);
-
-
-let restArr = ['Dhanush', ...actors];
-let [dhanush,vijay,...rest] = restArr;
-console.log(restArr);
-console.log(rest);
-
+//ARRAY
 
 //   ingayum andha ... adhan rest operator , then (munnadi ... use panna SPREAD)   (pinnadi ... use panna adhu REST)
 // REST => oru array la irundhu thaniya separate pannum.
+
+// let actors = ['Vijay','SivaKarthikeyan','Atharva'];
+
+// let updatedActor = [...actors, 'Dhanush','Suriya'];     // ... => this indicates spread operator
+// console.log(actors);
+// console.log(updatedActor);
+
+
+// let restArr = ['Dhanush', ...actors];
+// let [dhanush,vijay,...rest] = restArr;
+// console.log(restArr);
+// console.log(rest);
+
+
+
  
+//OBJECT
+
+let shalini = {
+	name :  'Shalini Venugopal',
+	age : 20
+}
+
+shalini = {
+	...shalini,   // SPREAD 
+	work : 'Software Developer'
+}
+
+let { age,...rest }  = shalini;        //{ age } => defines REST operator in object
+console.log(rest);
+
+
